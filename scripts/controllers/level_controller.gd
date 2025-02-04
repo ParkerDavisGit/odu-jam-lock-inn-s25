626720027c8f44fe1f764b8c19bdd9b17edd00cd
parker davis <theparkerevan>
2025-02-03 19:46:22 -0500

    misc

--- scripts/controllers/level_controller.gd
+++ scripts/controllers/level_controller.gd
@@ -1,19 +1,12 @@
 extends Node2D
 
-class_name Interactable
-
-var type: String
+class_name LevelController
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	type = "default"
+	pass # Replace with function body.
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
-
-
-func interact() -> void:
-	@warning_ignore("assert_always_true")
-	assert(true, "YOU FORGOT TO OVERWRITE INTERACT() IN %s YOU IDIOT" % type)
